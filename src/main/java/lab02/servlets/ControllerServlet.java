c338ed9f78c490b14109ae03e033eff8020de465
XyL1GaN4eG <trolloguy@mail.ru>
2024-11-15 16:05:29 +0300

    Update ControllerServlet.java

--- src/main/java/lab02/servlets/ControllerServlet.java
+++ src/main/java/lab02/servlets/ControllerServlet.java
@@ -1,43 +1,46 @@
 package lab02.servlets;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import lab02.commands.Delete;
 import lab02.commands.Get;
-import lab02.commands.Post;
 import lab02.data.RequestData;
 import lab02.exceptions.InvalidRequestException;
-import lab02.util.RequestDataAdapter;
+import lab02.util.RequestReader;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.charset.StandardCharsets;
 
 public class ControllerServlet extends HttpServlet {
-//    private Post post = new Post();
+    //todo: а может каждый хттп метод отдельным сервлетом сделать....
     private final Delete delete = new Delete();
     private final Get get = new Get();
-    private final Gson gson = new GsonBuilder()
-            .registerTypeAdapter(RequestData.class, new RequestDataAdapter()) //используем кастомный
-            .setPrettyPrinting()
-            .create();
+
+    private final RequestReader requestReader = new RequestReader();
 
     //todo: сделать так чтобы история хранилась в хттп сессии а не как сейчас
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //todo: добавить отправку клиенту
         get.execute();
     }
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        try {
+            var requestData = requestReader.getRequestData(request);
+            request.setAttribute("requestData", requestData);
+            //если реквестдата пришла без ошибок то закидываем реквест на сервлет areacheck
+            request.getRequestDispatcher("/AreaCheckServlet").forward(request, response);
+        } catch (InvalidRequestException e) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, String.valueOf(e));
+        }
     }
 
     @Override
     protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //todo: добавить отправку клиенту 200 ответа
         delete.execute();
     }
 
