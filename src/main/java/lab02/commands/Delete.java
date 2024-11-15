6c8d1b81aeb87474d5a7b5c87c85fbf295210b55
XyL1GaN4eG <trolloguy@mail.ru>
2024-11-15 18:09:32 +0300

    style: upd Delete.java

--- src/main/java/lab02/commands/Delete.java
+++ src/main/java/lab02/commands/Delete.java
@@ -6,7 +6,6 @@ import lab02.data.ResponseData;
 import java.util.List;
 
 public class Delete extends HttpCommand {
-
     @Override
     public List<ResponseData> execute() {
         history.clear();
