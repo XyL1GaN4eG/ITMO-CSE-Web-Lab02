42efb68410d6663f4c8c814d3d89451fdbdd6ee3
XyL1GaN4eG <trolloguy@mail.ru>
2024-11-15 14:42:45 +0300

    add support of lombok and javax.servlet

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("io.freefair.lombok") version "8.10"
 }
 
 group = "xyl1gan4eg"
@@ -12,6 +13,9 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("javax.servlet:javax.servlet-api:4.0.1")
+    implementation("com.google.code.gson:gson:2.11.0")
+
 }
 
 tasks.test {
