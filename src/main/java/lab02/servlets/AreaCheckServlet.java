ba0f49f1f29d527c0d5f96b9d732aae1c46a5b3f
XyL1GaN4eG <trolloguy@mail.ru>
2024-11-15 18:09:00 +0300

    feat: makes doPost in AreaCheckServlet.java

--- src/main/java/lab02/servlets/AreaCheckServlet.java
+++ src/main/java/lab02/servlets/AreaCheckServlet.java
@@ -1,17 +1,64 @@
 package lab02.servlets;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import lab02.data.RequestData;
+import lab02.data.ResponseData;
+import lab02.messaging.ResponseBuilder;
 import lab02.util.AreaChecker;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AreaCheckServlet extends HttpServlet {
     private final AreaChecker areaChecker = new AreaChecker();
+    private final DateTimeFormatter yyyymmddhhmmss = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    private final ResponseBuilder responseBuilder = new ResponseBuilder();
+    private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+        var requestDataObj = request.getAttribute("requestData");
+        var requestData = (RequestData) requestDataObj;
 
+        var handledRequestData = handleRequestData(requestData);
+        var json = gson.toJson(handledRequestData);
+
+         try (var out = response.getWriter()) {
+             out.println(responseBuilder.buildHttpResponseMessage(json, 200));
+             out.flush();
+         } catch (IOException e) {
+             e.printStackTrace();
+         }
+    }
+    //todo: добавить поддержку истории
+    private List<ResponseData> handleRequestData(RequestData requestsArray) {
+        var elementOfResponse = new ResponseData();
+        var responseArr = new ArrayList<ResponseData>();
+        for (int x : requestsArray.x()) {
+            elementOfResponse = createElementOfResponse(requestsArray, x);
+            responseArr.add(elementOfResponse);
+        }
+        return responseArr;
+    }
+
+    private ResponseData createElementOfResponse(RequestData requestsArray, int x) {
+        ResponseData response;
+        long startTime = System.nanoTime();
+        response = new ResponseData(x,
+                requestsArray.y(),
+                requestsArray.r());
+        response.setIn(areaChecker.validate(response));
+        response.setServerTime(LocalDateTime.now().format(yyyymmddhhmmss));
+        response.setExecutionTime(System.nanoTime() - startTime);
+        return response;
     }
 
 }
