5bbb79e9ba84d699045dcdeb1a0a14d20e4a554c
XyL1GaN4eG <trolloguy@mail.ru>
2024-11-16 19:42:11 +0300

    perf[RequestData]: switch from concat to stringbuilder

--- src/main/java/lab02/data/RequestData.java
+++ src/main/java/lab02/data/RequestData.java
@@ -3,13 +3,13 @@ package lab02.data;
 public record RequestData(int[] x, double y, double r) {
     @Override
     public String toString() {
-        var res = "x = ";
+        StringBuilder res = new StringBuilder("x = ");
         if (x != null) {
             for (int element : x) {
-                res += element + ", ";
+                res.append(element).append(", ");
             }
         }
-        res += "y = " + y + ", r = " + r;
-        return res;
+        res.append("y = ").append(y).append(", r = ").append(r);
+        return res.toString();
     }
 }
