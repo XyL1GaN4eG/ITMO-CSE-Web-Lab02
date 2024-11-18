1a1be48d65989f1f92545bf87684269a7cf4271e
XyL1GaN4eG <trolloguy@mail.ru>
2024-11-18 13:48:31 +0300

    feat: size of the graph depending on the radius

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -1,15 +1,32 @@
 //graph.js
 
-let radio = 1;
-let pointsByR = {};
-let board = JXG.JSXGraph.initBoard("jxgbox", {
-    boundingbox: [-2, 2, 2, -2],
-    axis: true
-});
+let board = createBoard(1); // Инициализация доски
 
 let graph1, graph2, graph3, i1, i2, i3;
 
-function redrawGraphs() {
+// Функция для создания новой доски
+function createBoard() {
+    const piece = 3;
+    return JXG.JSXGraph.initBoard("jxgbox", {
+        boundingbox: [-piece, piece, piece, -piece],
+        axis: true
+    });
+}
+
+function redrawGraphs(radio) {
+    // Удаляем старую доску
+    JXG.JSXGraph.freeBoard(board);
+
+    // Создаем новую доску с новыми параметрами
+    let rIn = parseFloat(radio);
+    if (rIn >= 1 && rIn <= 4) {
+        board = createBoard();
+    } else {
+        radio = 1
+        board = createBoard();
+    }
+
+    board.on('down', handleDown);
 
     if (graph1) board.removeObject(graph1);
     if (i1) board.removeObject(i1);
@@ -19,8 +36,9 @@ function redrawGraphs() {
     if (i3) board.removeObject(i3);
 
 
+    //triangle
     graph1 = board.create('functiongraph', [function (x) {
-        return 2 * x - 1;  // Уравнение прямой f(x) = 2x - 1
+        return 2 * x - radio;  // Уравнение прямой f(x) = 2x - 1
     }, 0, radio / 2]);  // Указываем диапазон x от -5 до 5
 
     i1 = board.create('integral', [[0, (radio / 2)], graph1], {
@@ -81,18 +99,22 @@ function redrawGraphs() {
         },
         fillColor: 'blue',
     });
-    // for (let r in pointsByR) {
-    //     pointsByR[r].forEach(point => point.setAttribute({visible: false}));
-    // }
-    //
-    //
-    // if (pointsByR[radio]) {
-    //     pointsByR[radio].forEach(point => point.setAttribute({visible: true}));
-    // }
+
 }
 
-
-redrawGraphs();
+var drawPoint = function (responseData) {
+    redrawGraphs(rInput.value)
+    for (let i = 0; i < responseData.length; i++) {
+        const jsonElement = responseData[i];
+        var point = board.create('point', [jsonElement.x, jsonElement.y]);
+        console.log(jsonElement.isIn === true);
+        if (jsonElement.isIn === true) {
+            point.setAttribute({color: 'green'})
+        } else {
+            point.setAttribute({color: 'red'})
+        }
+    }
+}
 
 //events.js
 function setupEventListeners(sendBtn, yInput, rInput, xCheckboxes, yError, rError) {
@@ -102,19 +124,32 @@ function setupEventListeners(sendBtn, yInput, rInput, xCheckboxes, yError, rErro
 
     yInput.addEventListener('input', () => {
         yTouched = true;
-        const { isYValid, isRValid, isXValid } = validateInputs(yInput, yError, rInput, rError, xCheckboxes, yTouched, rTouched);
+        const {
+            isYValid,
+            isRValid,
+            isXValid
+        } = validateInputs(yInput, yError, rInput, rError, xCheckboxes, yTouched, rTouched);
         sendBtn.disabled = !(isYValid && isRValid && isXValid);
     });
 
     rInput.addEventListener('input', () => {
         rTouched = true;
-        const { isYValid, isRValid, isXValid } = validateInputs(yInput, yError, rInput, rError, xCheckboxes, yTouched, rTouched);
+        const {
+            isYValid,
+            isRValid,
+            isXValid
+        } = validateInputs(yInput, yError, rInput, rError, xCheckboxes, yTouched, rTouched);
         sendBtn.disabled = !(isYValid && isRValid && isXValid);
+        redrawGraphs(rInput.value);
     });
 
     xCheckboxes.forEach(checkbox => {
         checkbox.addEventListener('change', () => {
-            const { isYValid, isRValid, isXValid } = validateInputs(yInput, yError, rInput, rError, xCheckboxes, yTouched, rTouched);
+            const {
+                isYValid,
+                isRValid,
+                isXValid
+            } = validateInputs(yInput, yError, rInput, rError, xCheckboxes, yTouched, rTouched);
             sendBtn.disabled = !(isYValid && isRValid && isXValid);
         });
     });
@@ -133,6 +168,9 @@ const yError = document.getElementById('y-error');
 const rError = document.getElementById('r-error');
 const xCheckboxes = document.querySelectorAll('#x-values input[type="checkbox"]');
 const url = "/lab02-1.0-SNAPSHOT/controller";
+
+redrawGraphs(rInput.value);
+
 
 async function sendData(xArray, y) {
     let r = "1.0"; // Устанавливаем значение по умолчанию для R
@@ -160,8 +198,9 @@ async function sendData(xArray, y) {
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify(obj) // Отправляем сформированный JSON
     });
-
-    return await response.json();
+    var responseData = await response.json();
+    generateTable(responseData)
+    drawPoint(responseData)
 }
 
 
@@ -205,14 +244,12 @@ sendBtn.addEventListener("click", async (event) => {
         x_values.push(element.value);
     });
 
-    var responseData = await sendData(
+    await sendData(
         x_values,
         yInput.value)
 
-    console.log("жсончик респонса")
-    console.log(responseData)
-
-    generateTable(responseData);
+    // generateTable(responseData);
+    // drawPoints(responseData);
 
 });
 
@@ -236,23 +273,24 @@ setupEventListeners(sendBtn, yInput, rInput, xCheckboxes, yError, rError);
 // Отключаем прокрутку
 document.body.style.overflow = 'hidden';
 
-function validatePoint(point) {
-
-}
-
 var getMouseCoords = function (e, i) {
         var pos = board.getMousePosition(e, i);
         return new JXG.Coords(JXG.COORDS_BY_SCREEN, pos, board);
     },
 
     handleDown = async function (e) {
+
+        console.log("1")
         var canCreate = true,
             i, coords, el;
+        console.log("2")
 
         if (e[JXG.touchProperty]) {
             // index of the finger that is used to extract the coordinates
             i = 0;
         }
+        console.log("3")
+
         coords = getMouseCoords(e, i);
 
         for (el in board.objects) {
@@ -261,16 +299,17 @@ var getMouseCoords = function (e, i) {
                 break;
             }
         }
+        console.log("точка")
+        console.log(coords)
         console.log("координаты точки")
-        console.log(coords.usrCoords[0], coords.usrCoords[1]);
+        console.log(coords.usrCoords[1], coords.usrCoords[2]);
 
         // var point = board.create('point', [coords.usrCoords[1], coords.usrCoords[2]]);
         // console.log(point)
 
-        var response = await sendData([coords.usrCoords[1]], coords.usrCoords[2])
-        generateTable(response);
+        // var response =
+        await sendData([coords.usrCoords[1]], coords.usrCoords[2])
+        // generateTable(response);
 
     };
-
-board.on('down', handleDown);
 
